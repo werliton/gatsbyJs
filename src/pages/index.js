b655a3a9ae2baf72164ac2b58148b97255bb14d8
Werliton Silva <werliton.carlos@ceuma.br>
2019-03-15 15:55:32 -0300

    Add new pages and using links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import React from "react"
+import { Link } from "gatsby";
 
 export default () => (
     <div>
         Working gatsby
         <button className="primary-button">Click me</button>
+        <ul>
+            <li><Link to="/" title="Home">Home</Link></li>
+            <li><Link to="/about/" title="About">About</Link></li>
+        </ul>
     </div>
 )
